30832f5dea1366f037515a449eadd01f9eb4d243
vsavko <35723434+vsavko@users.noreply.github.com>
2020-02-20 23:57:04 +0300

    Create DiagonalPuzzle.java

---
+++ RoundH2019/DiagonalPuzzle.java
@@ -0,0 +1,45 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class DiagonalPuzzle {
+	
+	private int solver(boolean[][] puzzle) {
+		boolean [] whiteDiagonals = new boolean[4*puzzle.length-2];
+		boolean [] usedDiagonals = new boolean[4*puzzle.length-2];
+		
+		//diagonals - 1 to 2n-1 -left, 2n to 4n-2 - right
+		//for right / - [0][0] - is first, [1][2] && [2][1] - second 
+		//for left \ - [n-1][0] is - 2n, [n-2][0] && [n-1][1] - is 2n +1 , etc...
+		
+		
+		
+	}
+	
+	private int getRightDiagonal(int x, int y) {
+		return x + y;
+	}
+	
+	private int getLeftDiagonal(int x, int y, int n) {
+		return (2*n - 1) + (n -x) +y;
+	}
+	
+	
+	
+	
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+        for (int i = 0; i < t; ++i) {
+          int n = in.nextInt();
+          int min = 0;
+
+          for (int j = 0 ; j < n; j++) { 
+        	  min = solver(in.nextInt(), pq, min);
+        	  sb.append(" ").append(min);
+          }
+          System.out.println(sb);
+        }
+      
+        in.close();
+      }
+}
