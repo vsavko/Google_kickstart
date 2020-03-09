f2fbc60aa0008765344d7b8aa0154df12d696b70
vsavko <35723434+vsavko@users.noreply.github.com>
2020-03-09 18:10:35 +0300

    Update Flattening.java

--- RoundF2019/Flattening.java
+++ RoundF2019/Flattening.java
@@ -1,108 +1,60 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
-public class Flattening {
+public class Flattening2 {
 	
-	public class Group{
-		int id;
-		int size;
-		int height;
-		int start;
-		int end;
+	public static int findMinChange(int [] array1, int limit) {
+		if (limit > array1.length) return 0;
+		int [][] cost = new int[array1.length+1][array1.length+1];
+		int [][] dp = new int[array1.length+1][array1.length+1];
 		
-		public Group(int id, int size, int height, int start, int end) {
-			super();
-			this.id = id;
-			this.size = size;
-			this.height = height;
-			this.start = start;
-			this.end = end;
-		}
-	}
-	
-	private int findMinChange(int [] array, int limit) {
-		int ans = 0;
-		int diff = 0;
-		Group [] arrayGrouped = new Group[array.length];
+		HashMap<Integer, Integer> hm;
 		
-		arrayGrouped[0] = new Group(0,1,array[0],0,0);
-		//find differences
-		for (int i = 1; i < array.length; i++) {
-			if (array[i-1] != array[i]) { 
-				++diff;
-				arrayGrouped[i] = new Group(i,1,array[i],i,i);
-			}
-			else {
-				++arrayGrouped[i-1].size;
-				arrayGrouped[i-1].end = i;
-				arrayGrouped[i] = arrayGrouped[i-1];
-			}
-			//arrayCopy[i-1] = array[i-1]; 
-			//if (i == array.length - 1 ) arrayCopy[i] = array[i]; 
-		}
-		//System.out.println(diff);
-		
-		//decrease min element(s)
-		while(diff > limit) {
-			int min = Integer.MAX_VALUE;
-			Group minGroup = null;
-			int k = 0;
-			for(int i = 0; i < array.length; i++) {
-				//chose min group, check if it has 2 same elements on the side
-				if (arrayGrouped[i].size < min) {
-					minGroup = arrayGrouped[i];
-					min = arrayGrouped[i].size;
-					k = 1; //decrease in difference
+		//find cost
+		for(int i = 0; i < array1.length; i++) {
+			
+			for(int j = i; j < array1.length; j++) {
+			   dp [i][j] = Integer.MAX_VALUE;
+			   hm = new HashMap<Integer, Integer>(); 
+			   for(int l = i; l <= j ; l++) {
+				   int key = array1[l]; 
+		           if(hm.containsKey(key)) hm.put(key, hm.get(key)+1); 
+		           else hm.put(key, 1); 
 				}
-				if (arrayGrouped[i].start > 0 && arrayGrouped[i].end < array.length-1 &&
-						arrayGrouped[i].size == min &&
-							arrayGrouped[arrayGrouped[i].start-1].height == arrayGrouped[arrayGrouped[i].end+1].height) { //if groups on two sides are equal
-					minGroup = arrayGrouped[i];
-					k = 2; //decrease in difference
+			   
+			    int max_count = 0; 
+		        for(Entry<Integer, Integer> val : hm.entrySet()) 
+		        { 
+		            if (max_count < val.getValue())
+		                max_count = val.getValue(); 
+		        } 
+		        cost[i][j] = j - i + 1 - max_count;
+			}
+		}
+		
+		//min dp[k][j-1] + cost [k+1][i] for all i
+		int min= Integer.MAX_VALUE;
+		for(int j = 0; j <=  limit; j++) {
+			for (int k = 0; k < array1.length; k++) { //k - divides the array into arbitrary two
+				if(j == 0 ) {
+					dp[k][j] = cost[0][k];
 				}
-				
-			}
-			//System.out.println("minGroup" + minGroup.id + " k " + k);
-			
-			if (minGroup == null) throw new IllegalArgumentException("minGroup is null!");
-			diff -= k;
-			
-			//add min group to left 
-			//System.out.println("minGroup.end" + (minGroup.end +1)+ " minGroup.start "+ minGroup.start);
-			if(minGroup.start - 1 > 0) {
-				//System.out.println("test1");
-				minGroup.height = arrayGrouped[minGroup.start - 1].height;
-			}
-			//to the right
-			else if (minGroup.end +1 < array.length) {
-				//System.out.println("test");
-				minGroup.height = arrayGrouped[minGroup.end + 1].height;
-			}
-			
-			//check groups
-			for(int i = 1; i < arrayGrouped.length; i++) {
-				//merge different neighbour groups with same height 
-				if(arrayGrouped[i].height == arrayGrouped[i-1].height &&
-						arrayGrouped[i].id != arrayGrouped[i-1].id ) {
-					arrayGrouped[i-1].size += arrayGrouped[i].size;
-					arrayGrouped[i-1].end = arrayGrouped[i].end;
-					//System.out.println(arrayGrouped[i].start + " " + arrayGrouped[i].end);
-					for(int j = arrayGrouped[i].start ; j <= arrayGrouped[i].end ; j++) {
-						//System.out.println(j + " " + arrayGrouped.length);
-						arrayGrouped[j] = arrayGrouped[i-1];
+				else {
+					min = Integer.MAX_VALUE;
+					int tmp =0;
+					for(int z = 0; z <= k; z++) {
+						tmp = dp[z][j-1] + cost[z+1][k];
+						if (min > tmp) min = tmp;
 					}
-				}
-			}
+					dp[k][j] = min;
+				}	
+			}	
 		}
-		//System.out.println();
-		//find how many elements were changed from initial values
-		for (int i = 0; i < array.length; i++) {
-			//System.out.printf("n %d, i %d id%d| ",arrayGrouped[i].height,array[i],arrayGrouped[i].id);
-			if (arrayGrouped[i].height != array[i])
-				++ans;
-		}
-		//System.out.println();
+
+		return dp[array1.length-1][limit];
 		
-		return ans;
 	}
 	
 	public static void main(String[] args) {
@@ -123,11 +75,12 @@ public class Flattening {
         	  array1[j] = tmp;
           }
           in.nextLine();
-
-          int min = (new Flattening()).findMinChange(array1, limit);
+          
+          int [] dynamic = new int[numInts];
+          Arrays.fill(dynamic, Integer.MAX_VALUE);
+          int min = findMinChange(array1, limit);
          
           System.out.println("Case #" + i + ": " + min );
-          //max = 0;
 
         }
         in.close();
